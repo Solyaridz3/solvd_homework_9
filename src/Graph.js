f902c2781934253c541bad0b7240e22250998fc8
Serhiy Vinarchuk <solyaridz3@gmail.com>
2024-05-21 16:34:29 +0300

    demo update

--- src/Graph.js
+++ src/Graph.js
@@ -21,7 +21,7 @@ class Edge {
     }
 }
 
-class Graph {
+export class Graph {
     _edgeClass = Edge;
     constructor() {
         this.vertices = {};
@@ -121,7 +121,7 @@ class WeightedEdge extends Edge {
     }
 }
 
-class WeightedGraph extends Graph {
+export class WeightedGraph extends Graph {
     constructor() {
         super();
         this.edgeClass = WeightedEdge;
@@ -177,48 +177,4 @@ class WeightedGraph extends Graph {
     }
 }
 
-const graph = new Graph();
-graph.insertVertex("a", "A");
-graph.insertVertex("b", "B");
-graph.insertVertex("c", "C");
-graph.insertVertex("d", "D");
-graph.insertVertex("e", "E");
-graph.insertVertex("f", "F");
-graph.insertVertex("g", "G");
 
-graph.insertEdges("a", ["b", "c"]);
-graph.insertEdges("b", ["f"]);
-graph.insertEdges("c", ["d", "e"]);
-graph.insertEdges("d", ["f"]);
-graph.insertEdges("e", ["f"]);
-graph.insertEdges("f", ["g"]);
-
-console.log("Default graph breadthSearch:", graph.breadthSearch("a", "g"));
-
-const weightedGraph = new WeightedGraph();
-
-weightedGraph.insertVertex("a", "ALLIGATOR");
-weightedGraph.insertVertex("b", "BAT");
-weightedGraph.insertVertex("c", "CAT");
-weightedGraph.insertVertex("d", "DOG");
-weightedGraph.insertVertex("f", "FROG");
-weightedGraph.insertVertex("g", "GIRAFFE");
-
-weightedGraph.insertEdges("a", { d: 2, f: 5, g: 7 });
-weightedGraph.insertEdges("d", { g: 2, f: 1, c: 10 });
-weightedGraph.insertEdges("g", { c: 2 });
-
-console.log("Weighted graph dijkstra algorithm:", weightedGraph.dijkstra("a"));
-console.log(
-    "Weighted graph breadth search",
-    weightedGraph.breadthSearch("a", "g")
-);
-
-//Initial weighted graph
-console.log(weightedGraph.vertices["a"].edges);
-
-weightedGraph.removeVertex("f");
-weightedGraph.removeEdge("a", "d");
-
-// Weighted graph after vertex and edge removal
-console.log(weightedGraph.vertices["a"].edges);
---
+++ src/demonstrations/graphDemo.js
@@ -0,0 +1,48 @@
+import { Graph, WeightedGraph } from "../Graph.js";
+
+
+const graph = new Graph();
+graph.insertVertex("a", "A");
+graph.insertVertex("b", "B");
+graph.insertVertex("c", "C");
+graph.insertVertex("d", "D");
+graph.insertVertex("e", "E");
+graph.insertVertex("f", "F");
+graph.insertVertex("g", "G");
+
+graph.insertEdges("a", ["b", "c"]);
+graph.insertEdges("b", ["f"]);
+graph.insertEdges("c", ["d", "e"]);
+graph.insertEdges("d", ["f"]);
+graph.insertEdges("e", ["f"]);
+graph.insertEdges("f", ["g"]);
+
+console.log("Default graph breadthSearch:", graph.breadthSearch("a", "g"));
+
+const weightedGraph = new WeightedGraph();
+
+weightedGraph.insertVertex("a", "ALLIGATOR");
+weightedGraph.insertVertex("b", "BAT");
+weightedGraph.insertVertex("c", "CAT");
+weightedGraph.insertVertex("d", "DOG");
+weightedGraph.insertVertex("f", "FROG");
+weightedGraph.insertVertex("g", "GIRAFFE");
+
+weightedGraph.insertEdges("a", { d: 2, f: 5, g: 7 });
+weightedGraph.insertEdges("d", { g: 2, f: 1, c: 10 });
+weightedGraph.insertEdges("g", { c: 2 });
+
+console.log("Weighted graph dijkstra algorithm:", weightedGraph.dijkstra("a"));
+console.log(
+    "Weighted graph breadth search",
+    weightedGraph.breadthSearch("a", "g")
+);
+
+//Initial weighted graph
+console.log(weightedGraph.vertices["a"].edges);
+
+weightedGraph.removeVertex("f");
+weightedGraph.removeEdge("a", "d");
+
+// Weighted graph after vertex and edge removal
+console.log(weightedGraph.vertices["a"].edges);
